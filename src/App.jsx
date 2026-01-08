20c77b7d816237729a474eed620c4d2b93735b16
Ranjan Mahato <i.ranjan1115@gmail.com>
2026-01-09 00:30:14 +0530

    new feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 import Navbar from "./components/Navbar";
@@ -6,13 +7,27 @@ import Resume from "./components/Resume";
 import ContactForm from "./components/ContactForm";
 import profile from "./assets/profile.jpg";
 
-
 const App = () => {
+  const fullName = "Ranjan Mahato !";
+  const [displayName, setDisplayName] = useState("");
+  const [index, setIndex] = useState(0);
+
+  // Typewriter effect (runs once on first load)
+  useEffect(() => {
+    if (index < fullName.length) {
+      const timeout = setTimeout(() => {
+        setDisplayName((prev) => prev + fullName[index]);
+        setIndex(index + 1);
+      }, 120); // typing speed (ms)
+
+      return () => clearTimeout(timeout);
+    }
+  }, [index, fullName]);
+
   return (
     <div className="app">
-        <header className="header">
+      <header className="header">
         <div className="header-inner">
-
           {/* Left image */}
           <img
             src={profile}
@@ -22,13 +37,16 @@ const App = () => {
 
           {/* Right text */}
           <div className="header-text">
-            <h1>Ranjan Mahato</h1>
+            <h1 className="typewriter">
+              {displayName}
+            </h1>
+
             <p>DevOps & Frontend Engineer</p>
           </div>
-
         </div>
       </header>
-        <Navbar /> 
+
+      <Navbar />
       <Skills />
       <Resume />
       <ContactForm />
